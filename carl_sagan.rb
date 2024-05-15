a1fa16f1ed6fd26f2f0d7becd9648b0b6677178f
varsanihemal <hemalvarsani123@gmail.com>
2024-05-15 08:46:16 -0500

    Script for hash of ghosts

--- carl_sagan.rb
+++ carl_sagan.rb
@@ -21,3 +21,17 @@ sagan = [
 example = [ 'test', 'a', 'is']
 time    = { :that => 'This', :period => '.'}
 puts "#{time[:that]} #{example[2]} #{example[1]} #{example[0]}#{time[:period]}"
+
+ghosts = [
+          { name: 'Inky', age: 4, loves: 'reindeers', net_worth: 25 },
+          { name: 'Pinky', age: 5, loves: 'garden tools', net_worth: 14 },
+          { name: 'Blinky', age: 7, loves: 'ninjas', net_worth: 18.03 },
+          { name: 'Clyde', age: 6, loves: 'yarn', net_worth: 0 }
+        ]
+
+ghosts.each do |ghost|
+    ghost_info  = "#{ghost[:name]} is #{ghost[:age]} years old, "
+    ghost_info += "loves #{ghost[:loves]} and "
+    ghost_info += "has #{ghost[:net_worth]} dollars in the bank."
+    puts ghost_info
+end
